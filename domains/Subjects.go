7d3727babda8f92b0ecdd99b5831f7d560300d48
Unknown <bartkovasvetlana@gmail.com>
2019-04-11 20:32:54 +0300

    Added CRUD for subject

--- domains/Subjects.go
+++ domains/Subjects.go
@@ -4,7 +4,21 @@ import (
 	"net/http"
 	"encoding/json"
 	"../utils"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"os"
 )
+var log = logrus.New()
+
+func PlugLog() {
+	log.Out = os.Stdout
+	file, err := os.OpenFile("session.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err == nil {
+		log.Out = file
+	} else {
+		log.Info("Failed to log to file")
+	}
+}
 
 
 type Subject struct {
@@ -17,4 +31,56 @@ func GetSubjects(w http.ResponseWriter, r *http.Request){
 	var subjects []Subject
 	db.Find(&subjects)
 	json.NewEncoder(w).Encode(&subjects)
+}
+
+func CreateSubject (w http.ResponseWriter, r *http.Request){
+	db := utils.GetDB()
+	var subject Subject
+	json.NewDecoder(r.Body).Decode(&subject)
+	db.Create(&subject)
+	json.NewEncoder(w).Encode(&subject)
+	json.NewEncoder(w).Encode(&subject)
+	PlugLog()
+	log.WithFields(logrus.Fields{
+		"subject_id":    subject.Subject_id,
+		"subject_name":  subject.Subject_name,
+	}).Info("Created subject")
+}
+
+func DeleteSubject(w http.ResponseWriter, r *http.Request) {
+	db := utils.GetDB()
+	params := mux.Vars(r)
+	var subject Subject
+	db.First(&subject, params["id"])
+	db.Delete(&subject)
+
+	var subjects []Subject
+	db.Find(&subjects)
+	json.NewEncoder(w).Encode(&subjects)
+	json.NewEncoder(w).Encode(&subjects)
+	PlugLog()
+	log.WithFields(logrus.Fields{
+		"subject_id":    subject.Subject_id,
+	}).Info("Deleted subject")
+}
+
+func UpdateSubject(w http.ResponseWriter, r *http.Request) {
+
+	db := utils.GetDB()
+
+	params := mux.Vars(r)
+	var updSubject Subject
+	db.First(&updSubject, params["id"])
+	json.NewDecoder(r.Body).Decode(&updSubject)
+	if err := db.Save(updSubject); err.Error != nil {
+		return
+	}
+	var subjects []Subject
+	db.Find(&subjects)
+	json.NewEncoder(w).Encode(&subjects)
+	PlugLog()
+	log.WithFields(logrus.Fields{
+		"subject_id":    updSubject.Subject_id,
+		"subject_name":  updSubject.Subject_name,
+	}).Info("Updated subject")
 }
--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 	"log"
 )
 
-
-
-
 func main() {
 	router := mux.NewRouter()
 	utils.InitDB()
 
-	router.HandleFunc("/subjects", domains.GetSubjects).Methods("Get")
+	router.HandleFunc("/subjects", domains.GetSubjects).Methods("GET")
+	router.HandleFunc("/subjects", domains.CreateSubject).Methods("POST")
+	router.HandleFunc("/subjects/{id}", domains.DeleteSubject).Methods("DELETE")
+	router.HandleFunc("/subjects/{id}", domains.UpdateSubject).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
